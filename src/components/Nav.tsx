8e89776ef24638dd9fb4aaf781946a82bdd91874
Poshuk Vadym <vadimposhuk1999@gmail.com>
2023-08-11 12:34:02 +0200

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "./../img/Logo.png";
+
+function Nav() {
+  return (
+    <nav className="Nav">
+      <div className="container">
+        <div className="Nav-wrapper">
+          <div className="logo">
+            <a href="./">
+              <img src={logo} className="logo" alt="logo" />
+            </a>
+          </div>
+          <ul className="Nav-menu">
+            <a href="./index.html">
+              <li className="active">Головна</li>
+            </a>
+            <a href="./index.html">
+              <li>Фото</li>
+            </a>
+            <a href="./index.html">
+              <li>Про нас</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
